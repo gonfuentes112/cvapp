d0bb8ac5de3c12532e95b532c9f6ed9b179f68a7
gekishou <gonfuentes112@gmail.com>
2025-01-07 22:32:36 -0300

    feat: turn app into grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 // import { useState } from 'react'
 import './App.css'
+import Forms from './components/forms'
+import Cv from './components/cv'
+
 
 function App() {
 
   return (
     <>
+    <Forms></Forms>
+    <Cv></Cv>
     </>
   )
 }
---
+++ src/components/forms.jsx
@@ -0,0 +1,5 @@
+function Forms() {
+    return <div></div>
+}
+
+export default Forms
